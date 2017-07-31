80e2cc34b6cec043f0facb6d04d06443509d0a26
wind <aszx4510@yahoo.com.tw>
2017-08-01 05:38:17 +0800

    Modify code structure at data_cleaning.py, add some functions for training data and testing data cleaning

--- data_cleaning.py
+++ data_cleaning.py
@@ -5,7 +5,37 @@ from corpus_loader import *
 nltk.data.path.append('nltk_data/')
 
 
-def make_w2v_data(reviews):
+def clean_training_data(lines):
+    lemmatizer = WordNetLemmatizer()
+
+    output_lines = list()
+    for id, sentiment, review in lines:
+        print(id)
+        review = review.strip('"')
+        review = re.sub('(<br \/>)+', ' ', review)  # replace <br /> to space
+        review = review.replace('\\"', '"')
+        tokens = nltk.word_tokenize(review)
+        tokens = [lemmatizer.lemmatize(token) for token in tokens]
+        output_lines.append(id + '\t' + sentiment + '\t' + ' '.join(tokens) + '\n')
+    return output_lines
+
+
+def clean_training_data(lines):
+    lemmatizer = WordNetLemmatizer()
+
+    output_lines = list()
+    for id, review in lines:
+        print(id)
+        review = review.strip('"')
+        review = re.sub('(<br \/>)+', ' ', review)  # replace <br /> to space
+        review = review.replace('\\"', '"')
+        tokens = nltk.word_tokenize(review)
+        tokens = [lemmatizer.lemmatize(token) for token in tokens]
+        output_lines.append(id + '\t' + ' '.join(tokens) + '\n')
+    return output_lines
+
+
+def generate_w2v_data(reviews):
     sentences = list()
     for review in reviews:
         review = re.sub('(<br \/>)+', ' ', review)  # replace <br /> to space
@@ -31,12 +61,23 @@ def sentences_preprocessing(sentences):
     return token_lemma_sentences
 
 
-def output_w2v_file(output_path, sentences):
+def output_file(output_path, lines):
     with open(output_path, 'w', encoding='utf8', newline='\n') as out:
-        out.writelines(sentences)
+        out.writelines(lines)
 
 
 if __name__ == '__main__':
+    # # clean training data
+    # lines = load_training_data('data/labeledTrainData.tsv')
+    # output_lines = clean_training_data(lines)
+    # output_file(output_path='dataOutput/labeledTrainData_clean.tsv', lines=output_lines)
+
+    # # clean testing data
+    # lines = load_testing_data('data/testData.tsv')
+    # output_lines = clean_training_data(lines)
+    # output_file(output_path='dataOutput/testData_clean.tsv', lines=output_lines)
+
+    # generate w2v training file
     lines = load_training_data('data/labeledTrainData.tsv')
-    sentences = make_w2v_data([line[2].strip('"') for line in lines])
-    output_w2v_file('dataOutput/labeledTrainData_sentences.txt', sentences=sentences)
+    sentences = generate_w2v_data([line[2].strip('"') for line in lines])
+    output_file('dataOutput/labeledTrainData_sentences.txt', lines=sentences)
