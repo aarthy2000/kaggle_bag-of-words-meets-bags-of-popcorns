2516ab98c5a3db7e0a307e728f4a8266c36526de
wind <aszx4510@yahoo.com.tw>
2017-08-01 04:47:12 +0800

    Add tokenization and lemmatization from nltk

--- make_w2v_data.py
+++ make_w2v_data.py
@@ -1,5 +1,8 @@
 import re
+import nltk
+from nltk.stem import WordNetLemmatizer
 from corpus_loader import *
+nltk.data.path.append('nltk_data/')
 
 
 def make_w2v_data(reviews):
@@ -10,9 +13,22 @@ def make_w2v_data(reviews):
         review_sentences = re.split('(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?|!)\s', review)  # split to sentences
         # append \n and remove space
         review_sentences = [(sentence + '\n').strip(' ') for sentence in review_sentences]
-        print(len(review_sentences))
         sentences.extend(review_sentences)
+
+    sentences = sentences_preprocessing(sentences=sentences)
     return sentences
+
+
+def sentences_preprocessing(sentences):
+    lemmatizer = WordNetLemmatizer()
+
+    token_lemma_sentences = list()
+    for sentence in sentences:
+        tokens = nltk.word_tokenize(sentence)
+        tokens = [lemmatizer.lemmatize(token) for token in tokens]
+        token_lemma_sentences.append(' '.join(tokens) + '\n')
+
+    return token_lemma_sentences
 
 
 def output_w2v_file(output_path, sentences):
